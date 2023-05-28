f9833b8f80be92a610a9c0faacae2070cafdb530
maty7dub <matyas-s@seznam.cz>
2023-05-28 18:36:02 +0200

    Added todo

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ game = Game()
 while game.running:
     game.curr_menu.display_menu()
     game.game_loop()
+
+#TODO:
+#fix the mouse going into the fucking floor
+#fix the cat going into the fucking walls
+#fix the cat going into fucking orbit
+#fix the funny-ahh physics
